78b7418cdbe83253eff7a3d8c2ae23b0f82cd765
Javier de Salas Lattuada <javidesalas@gmail.com>
2023-08-16 19:28:31 +0200

    Update sellers

--- models/entry.model.js
+++ models/entry.model.js
@@ -1,33 +1,34 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const entrySchema = new Schema({
-    firstName: String,
-    lastName: String,
-    birthDate: Date,
-    zipCode: Number,
-    email: String,
-    phone: String,
-    code: String,
-    boughtAt: {
-        type: String,
-        enum: [
-            'Alcampo',
-            'Carrefour',
-            'Consum',
-            'Dia',
-            'ECI',
-            'Eroski',
-            'Froiz',
-            'Gadisa',
-            'Otros'
-        ]
-    },
-    imageUrl: String,
-    acceptedTerms: Boolean
-}, {
-    timestamps: true
-})
+const entrySchema = new Schema(
+	{
+		firstName: String,
+		lastName: String,
+		birthDate: Date,
+		zipCode: Number,
+		email: String,
+		phone: String,
+		code: String,
+		boughtAt: {
+			type: String,
+			enum: [
+				"Ahorramas",
+				"Alcampo",
+				"BonPreu",
+				"Carrefour",
+				"ECI",
+				"Eroski",
+				"Otros",
+			],
+		},
+		imageUrl: String,
+		acceptedTerms: Boolean,
+	},
+	{
+		timestamps: true,
+	}
+);
 
 const Entry = mongoose.model('Entry', entrySchema)
 module.exports = Entry
